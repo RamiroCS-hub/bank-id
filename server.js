7529117b898c772809173f427e82c8d7343ba0ab
Rama <ramirocarnicersouble8@gmail.com>
2024-05-14 21:32:40 -0300

    Nodemon Added

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import dotenv from 'dotenv'
 import express, { json } from 'express';
 const app = express();
 import cors from 'cors';
-const PORT = process.env.PORT || 3001;
-import { sequelize } from './config/database';
+import router from './routes/user.js';
+//import { sequelize } from './config/database.js';
 
 dotenv.config();
 app.use(json());
 app.use(cors({ origin: true, credentials:true }));
+
+app.use('/user', router)
 
 app.use("/", (req, res) => {
     res.json({
@@ -16,9 +18,12 @@ app.use("/", (req, res) => {
     })
 });
 
+/*
 sequelize.sync().then(() => {
     console.log('Database synced succesfully');
-})
+})*/
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`server is listening on http://localhost:${PORT}`);
