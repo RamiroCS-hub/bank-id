1ab3ede0c13ac451f395b4d44d0f23983eea2816
Rama <ramirocarnicersouble8@gmail.com>
2024-05-15 22:39:51 -0300

    User login without JWT added

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,22 @@
-import { validateUser, checkCredentials } from "../services/service.js";
+import { validateUser, getUserId, checkCredentials } from "../services/service.js";
 
 export class UserController{
   static async loginUser (req, res){
-    const isValid = validateUser(req.body);
+    try {
+      const isValid = validateUser(req.body);
+      console.log(`El body es: ${req.body.cardNumber}, ${req.body.password}`);
 
-    if(!isValid.success) return res.send({message: 'The data is invalid', error: isValid.error});
-    
-    const user = await checkCredentials(req.body.cardNumber);
-    console.log(user);
+      if(!isValid.success) return res.send({message: 'The data is invalid', error: isValid.error});
+      
+      const userId = await getUserId(req.body.cardNumber)
+      if(userId == -1) return res.status(400).send({ message: 'Invalid card number'});
+
+      const user = await checkCredentials(req.body.password, userId)
+      if(user == -1) return res.status(400).send({ message: 'Incorrect Password'}); 
+      
+      return res.status(200).send({ message: 'You login successfully'});
+    } catch (err) {
+      return res.status(400).send({ error: err.message});
+    }
   }
 }
--- services/service.js
+++ services/service.js
@@ -3,24 +3,37 @@ import { Card, User } from '../models/index.js';
 
 const userValidation = z.object({
   cardNumber: z.number().int(),
-  password: z.string()
+  password: z.number().int()
 })
 
 export function validateUser(object){
   return userValidation.safeParse(object);
 }
 
-export async function checkCredentials(cardNumber){
+export async function getUserId(cardNumber){
   //Devolver el usuario que matchea con el cardNumber
   try{
     /*const result = await User.create({ firstname: 'Pepe', lastname: 'Perez', email:'pepe@gmail.com', pin: 1123 });
     console.log('El result fue', result);
     const card = await Card.create({ cardNumber: 123456, isAuth: true, userId: 1});
     console.log(card);*/
-    const result = await User.findOne({ })
-
+    const { userId, isAuth } = await Card.findOne({ where: {cardNumber: cardNumber } });
+    if(!isAuth) return -1;
+    return userId;
   }catch(e){
     console.log("Error: " + e);
+    throw e;
   }
-  
+}
+
+export async function checkCredentials(password, userId) {
+  try {
+    const { pin } = await User.findOne({ where: { id: userId } });
+
+    if(pin != password) return -1;
+    return 0
+  } catch (e) {
+    console.log("Error: " + e);
+    throw e;
+  }
 }
