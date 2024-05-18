62cfd50913c304d13e13cad8845aad02fbfaa368
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2024-05-18 10:40:02 -0400

    update models index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 //import { foreignKey } from 'sequelize/lib/query-types';
 import {Card} from './card.js';
 import {User} from './user.js';
+import {Transaction} from './transaction.js'
 
 User.hasMany(Card, { foreignKey: {name: 'userId'}})
 Card.belongsTo(User, { foreignKey: {name: 'userId'}})
 
-export { Card, User }
+Transaction.belongsTo(Card, { foreignKey: {name: 'cardId'} })
+Card.hasMany(Transaction, { foreignKey: {name: 'cardId'}})
+
+export { Card, User, Transaction }
