8e3a6207db30ecf4a31fe971833a1d67477c0f58
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2024-05-18 10:41:04 -0400

    add transactionRouter to index routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import userRouter from "./user.js";
 import cardRouter from "./card.js";
+import transactionRouter from './transaction.js'
 
-export { cardRouter, userRouter }
+export { cardRouter, userRouter, transactionRouter }
