a7c9742f889d2dd62eca7431e40cd35512d1e2fc
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2024-05-21 21:27:23 -0400

    db logic added to service

--- services/transactionService.js
+++ services/transactionService.js
@@ -1,4 +1,6 @@
 import z from "zod";
+import { Card, Transaction } from '../models/index.js';
+import { TransactionError, ValidationError } from '../utils/utils.js';
 
 const transactionValidation = z.object({
   cardNumberOrigin: z.number().int(),
@@ -12,10 +14,61 @@ const cardNumberValidation = z.object({
 })
 
 export function validateTransaction(object) {
-  return transactionValidation.safeParse(object);
+  return transactionValidation.parse(object);
 }
 
 export async function validateGetTransactions(object) {
-  return cardNumberValidation.safeParse(object)
+  return cardNumberValidation.parse(object)
 }
+
+export async function getAllTransaction(data) {
+  try {
+    const { cardNumber } = data;
+    const card = await Card.findOne({where: { cardNumber: cardNumber }})
+
+    if (!card) throw new TransactionError("Card not found");
+
+    const transactions = await Transaction.findAll({where: { cardId: card.id }})
+    return transactions
+  } catch (error) {
+    return new ValidationError(error.message);
+  }
+}
+
+
+export async function createTransaction(data) {
+  try {
+    const { cardNumberOrigin, cardNumberDestination, amount, description } = data;
+
+    const cardOrigin = await Card.findOne({ where: { cardNumber: cardNumberOrigin } });
+    const cardDestination = await Card.findOne({ where: { cardNumber: cardNumberDestination } });
+
+    if (!cardOrigin || !cardDestination) {
+      return new ValidationError('Invalid card numbers. Please check.');
+    }
+
+    const originAmount = cardOrigin.amount;
+    const destinationAmount = cardDestination.amount;
+    
+    if (amount > originAmount) {
+      return new ValidationError('Insufficient funds. Please check your balance account.');
+    }
+
+    await Card.update({ amount: originAmount - amount }, { where: { id: cardOrigin.id } });
+    await Card.update({ amount: destinationAmount + amount }, { where: { id: cardDestination.id } });
+
+    const transaction = await Transaction.create({
+      cardId: cardOrigin.id,
+      amount,
+      description,
+      cardNumberOrigin,
+      cardNumberDestination
+    });
+
+    return transaction;
+  } catch (error) {
+    return new ValidationError(error.message);
+  }
+}
+
 
