78cbd14d777d15f0f469fdfc2cdf92abbacd9e2d
Rama <ramirocarnicersouble8@gmail.com>
2024-05-29 14:28:46 -0300

    Clean Code

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -9,25 +9,24 @@ export class TransactionController {
 
   static async getAllTransaction(req, res) {
     const isValid = validateGetTransactions(req.body);
-    if (!isValid.success) return res.send({ message: 'Data is invalid', error: isValid });
+    if (!isValid.success) return res.send({ message: 'Data is invalid', error: isValid })
       
-    const transactions = await getAllTransaction(req.body);
+    const transactions = await getAllTransaction(req.body)
     if(transactions instanceof ValidationError) return res.status(400).send({ message: transactions.message})
 
     return res.send(transactions)
   }
 
-
   static async createTransanction(req, res) {
     // En este todas las validaciones funcionan correctamente
-    const isValid = validateTransaction(req.body);
-    console.log(isValid);     
+    const isValid = validateTransaction(req.body)
+    console.log(isValid)
     if(!isValid.success) {
       const message = isValid.error.issues.map((e) => e)
-      return res.send({ message: 'Data is invalid', error: message });
+      return res.send({ message: 'Data is invalid', error: message })
     }
-    const transaction = await createTransaction(req.body);
+    const transaction = await createTransaction(req.body)
     if(transaction instanceof ValidationError) return res.status(400).send({ message: transaction.message})
-    return res.send(transaction);
+    return res.send(transaction)
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,6 @@ import { UserController } from '../controllers/user.js';
 import { verifyAdmin, verifyToken } from '../middlewares/auth.js';
 const userRouter = express.Router();
 
-userRouter.get('/', (req, res) => {
-  res.send('This is the user get router');
-})
-
 userRouter.post('/login', UserController.loginUser)
 userRouter.post('/testUser', verifyAdmin, UserController.createUser)
 userRouter.post('/logout', verifyToken, UserController.logoutUser)
